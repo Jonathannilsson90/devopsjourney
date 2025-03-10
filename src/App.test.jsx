249f319e7090ccb7c46fabb3d7b38b9defa069b3
JonathanNilsson90 <koffeinmissbrukarn@gmail.com>
2025-03-10 12:51:40 +0100

    init commit

---
+++ src/App.test.jsx
@@ -0,0 +1,21 @@
+import {fireEvent, render, screen} from '@testing-library/react'
+import App from './App'
+import { expect } from 'vitest';
+
+it('Should render h1 with Vite + React', () => {
+  render(<App/>);
+  const headingElement = screen.getByText('Vite + React');
+  expect(headingElement).toBeVisible();
+})
+
+it('Should press the button and increment count', () => {
+render(<App/>)
+const buttonElement = screen.getByRole('button', {name: /count is/i})
+  expect(buttonElement).toBeVisible();
+
+  expect(buttonElement).toHaveTextContent('count is 0');
+
+  fireEvent.click(buttonElement)
+
+  expect(buttonElement).toHaveTextContent('count is 1')
+})
